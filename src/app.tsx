ce070de27e5c6610b220d3841751f67d57307579
joje3029 <joje3029@gmail.com>
2025-06-20 23:22:36 +0900

    가상 돔 생성

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,25 @@
 // ---- 도서관 ---
+const React = {
+  createElement: (tag, props, ...children) => {
+    // Virtual DOM 객체를 생성하여 반환
+    const el = {
+      tag, //HTML 태그 이름(div, h2, p, input 등)
+      props, //속성들(draggable, type 등)
+      children //자식 요소들(h2, p, input 등)
+    };
+    
+    console.log('Created Virtual DOM:', el); // 콘솔에 찍으면 가상 돔 객체 출력
+    return el;
+  }
+};
 
 // ---- 애플리케이션 ---
+const App = (
+    <div draggable>
+      <h2>Hello React!</h2>
+      <p>I am a pargraph</p>
+      <input type="text" />
+    </div>
+  );
 
-console.log('Hello World'); 
+console.log(App); 
